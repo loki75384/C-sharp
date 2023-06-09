c948ba61322b05c0d6cfef0d7948aee26252c360
loki47 <larsentuta49@gmail.com>
2023-06-10 01:49:18 +0300

    Example007_Magic

--- Example007_Magic/Program.cs
+++ Example007_Magic/Program.cs
@@ -5,20 +5,20 @@ int xa = 40, ya = 1,
     xb = 1, yb = 30,
     xc = 80, yc = 30;
 
-//Console.SetCursorPosition(xa, ya);
-//Console.WriteLine("+");
+Console.SetCursorPosition(xa, ya);
+Console.WriteLine("+");
 
-//Console.SetCursorPosition(xb, yb);
-//Console.WriteLine("+");
+Console.SetCursorPosition(xb, yb);
+Console.WriteLine("+");
 
-//Console.SetCursorPosition(xc, yc);
-//Console.WriteLine("+");
+Console.SetCursorPosition(xc, yc);
+Console.WriteLine("+");
 
 int x = xa, y = xb;
 
 int count = 0;
 
-while(count < 100);
+while(count < 10000 );
 {
     int what = new Random().Next(0, 3); // [0, 3] 0 1 2
     if(what == 0)
